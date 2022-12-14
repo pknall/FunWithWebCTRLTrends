7e09b93fb5515b2ab5450d9cf69955d4baa87a64
pxknall <peterknall@hotmail.com>
2022-12-14 11:59:56 -0500

    Work on Trend Exporting Servlet.

--- src/main/java/com/ccgautomation/servlets/TrendServlet.java
+++ src/main/java/com/ccgautomation/servlets/TrendServlet.java
@@ -1,16 +1,10 @@
 package com.ccgautomation.servlets;
 
 import com.ccgautomation.trends.MyAnalogTrendProcessor;
-import com.ccgautomation.trends.MyAnalogTrendSample;
-import com.ccgautomation.trends.MyDigitalTrendProcessor;
-import com.ccgautomation.trends.MyEquipmentColorTrendProcessor;
 import com.controlj.green.addonsupport.access.*;
 import com.controlj.green.addonsupport.access.aspect.AnalogTrendSource;
-import com.controlj.green.addonsupport.access.aspect.DigitalTrendSource;
-import com.controlj.green.addonsupport.access.aspect.EquipmentColorTrendSource;
 import com.controlj.green.addonsupport.access.aspect.TrendSource;
 import com.controlj.green.addonsupport.access.trend.*;
-import org.jetbrains.annotations.NotNull;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,15 +14,18 @@ import java.io.IOException;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
+// https://happycoding.io/tutorials/java-server/rest-api - Rest Servlet
+// https://stackoverflow.com/questions/31657641/tomcat-restful-web-service-deployment - REST Tomcat Servlet
 // http://alcshare.com/content/add-on-tutorial/trends.html
 // https://github.com/alclabs/ZoneHistory/blob/master/src/main/java/com/controlj/addon/zonehistory/reports/EnvironmentalIndexProcessor.java
 // https://github.com/alclabs/ZoneHistory/blob/master/src/main/java/com/controlj/addon/zonehistory/reports/EnvironmentalIndexReport.java
 
 public class TrendServlet extends HttpServlet {
 
+    // ABSPATH:1:#wp_ccg_fpvav-1-11/max_heating_cfm_trend
+    private static final String ABSPATH  = "ABSPATH:1:#";
     private static String[] ids = {"ABSPATH:1:#wp_ccg_fpvav-1-11/max_heating_cfm_trend"};
     private static final Date startDate = new Date(1664582400000L);
     private static final Date endDate = new Date(1664668800000L);
@@ -41,12 +38,20 @@ public class TrendServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try
         {
+            String requestUrl = request.getRequestURI();
+            String[] fields = requestUrl.split("/");
             response.setContentType("text/plain");
             Writer writer = response.getWriter();
             //disableCache(response);
 
             TrendRange range = TrendRangeFactory.byDateRange(startDate, endDate);
             writer.write(doWork(ids, new MyAnalogTrendProcessor(), range));
+            writer.write(requestUrl);
+            for (String s : fields) {
+                writer.write(">");
+                writer.write(s);
+                writer.write("\r\n");
+            }
         }
         catch (Exception e)
         {
